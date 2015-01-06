58c10a33b3e0475a2c50c5286dd19f0b914c6fe9
Thoronador <thoronador@users.sourceforge.net>
2015-01-06 20:15:36 +0100

    add some simple test cases

---
+++ tests/config/config_test_create.cpp
@@ -0,0 +1,59 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the Private Message Database.
+    Copyright (C) 2015  Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+#include "../../Config.hpp"
+
+int main(void)
+{
+  Config conf;
+
+  // all stuff should be empty at the beginning
+  if (!conf.getForumURL().empty())
+  {
+    std::cout << "Error: forum URL is not empty!\n";
+    return 1;
+  }
+  if (!conf.getTPL().empty())
+  {
+    std::cout << "Error: TPL file name is not empty!\n";
+    return 1;
+  }
+  if (!conf.getSmilies().empty())
+  {
+    std::cout << "Error: smilie list is not empty!\n";
+    return 1;
+  }
+  //all checks successful
+  return 0;
+}
+
+/*
+
+BOOST_AUTO_TEST_CASE (config_setTPL)
+{
+  Config conf;
+  conf.setTPLFile("/some/nice/path/to/file.tpl");
+  // tpl file name should match value above
+  BOOST_CHECK(conf.getTPL() == "/some/nice/path/to/file.tpl");
+}
+
+BOOST_AUTO_TEST_SUITE_END( )
+*/

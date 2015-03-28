24e2332dbfa64b318f218d37b6a3f9ea0e6fbbe3
Thoronador <thoronador@users.sourceforge.net>
2015-03-28 19:37:13 +0100

    add test for ListBBCode class

---
+++ tests/bbcode/list_bbcode_test.cpp
@@ -0,0 +1,151 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the Private Message Database test suite.
+    Copyright (C) 2015  Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+#include <map>
+#include "../../code/bbcode/ListBBCode.hpp"
+
+int main()
+{
+  //unordered list code test
+  {
+    //some rather standard list code
+    ListBBCode listCode_unordered("list", true);
+
+    // Populate map with strings for testing.
+    //   Keys:   input string
+    //   Values: expected output string
+    std::map<std::string, std::string> tests;
+    //empty string stays unchanged
+    tests[""] = "";
+    //unchanged, no code
+    tests["There is no list here."] = "There is no list here.";
+    //minimalistic list example
+    tests["[list][*]item[/list]"] = "<ul><li>item</li></ul>";
+    //simple list with multiple items
+    tests["This is a list. [list][*]one[*]two[*]three[/list]"] = "This is a list. <ul><li>one</li><li>two</li><li>three</li></ul>";
+    //simple list with multiple items - upper case codes
+    tests["This is a list. [LIST][*]one[*]two[*]three[/LIST]"] = "This is a list. <ul><li>one</li><li>two</li><li>three</li></ul>";
+    //simple list with multiple items - upper case first letter in codes
+    tests["This is a list. [List][*]one[*]two[*]three[/List]"] = "This is a list. <ul><li>one</li><li>two</li><li>three</li></ul>";
+    //simple list with multiple items - upper and lower case alternate
+    tests["This is a list. [LiSt][*]one[*]two[*]three[/lIsT]"] = "This is a list. <ul><li>one</li><li>two</li><li>three</li></ul>";
+    //incomplete list - code should refuse to change text
+    tests["Incomplete [list][*]one[*]two[*]three"] = "Incomplete [list][*]one[*]two[*]three";
+    //list without items should not be parsed
+    tests["List without items [list]incoming[/list]"] = "List without items [list]incoming[/list]";
+    //Nested lists should be just fine.
+    tests["I heard you like lists, so [list][*]I [list][*]made[*]a[*]list[/list][*]inside[*]your list.[/list]"]
+          = "I heard you like lists, so <ul><li>I <ul><li>made</li><li>a</li><li>list</li></ul></li><li>inside</li><li>your list.</li></ul>";
+    //list with line breaks between items - line breaks should not be touched here
+    tests[std::string("Hey!\n")
+          +"[list]\n"
+          +"[*]once\n"
+          +"[*]there\n"
+          +"[*]was a\n"
+          +"[*]crash test dummy,\n"
+          +"[*]who felt crashs ain't funny.\n"
+          +"[/list]"] = std::string("Hey!\n<ul>\n<li>once\n</li><li>there\n</li><li>was a\n</li>")
+                       +"<li>crash test dummy,\n</li><li>who felt crashs ain't funny.\n</li></ul>";
+
+    //iterate over all given strings and check, if they get the expected result
+    std::map<std::string, std::string>::const_iterator iter = tests.begin();
+    while (iter != tests.end())
+    {
+      std::string text = iter->first;
+      listCode_unordered.applyToText(text);
+      if (text != iter->second)
+      {
+        std::cout << "Error: Some code was not transformed properly!\n"
+                  << "Instance: ListBBCode(\"list\", true)\n"
+                  << "Original text:   \""<<iter->first<<"\"\n"
+                  << "Expected result: \""<<iter->second<<"\"\n"
+                  << "Actual result:   \""<<text<<"\"\n";
+        return 1;
+      }
+      ++iter;
+    } //while
+    std::cout << "Passed all unordered list code tests.\n";
+  } //end of scope for unordered list tests
+
+
+  //unordered list code test
+  {
+    //ordered list code
+    ListBBCode listCode_ordered("list", false);
+
+    // Populate map with strings for testing.
+    //   Keys:   input string
+    //   Values: expected output string
+    std::map<std::string, std::string> tests;
+    //empty string stays unchanged
+    tests[""] = "";
+    //unchanged, no code
+    tests["There is no list here."] = "There is no list here.";
+    //minimalistic list example
+    tests["[list][*]item[/list]"] = "<ol><li>item</li></ol>";
+    //simple list with multiple items
+    tests["This is a list. [list][*]one[*]two[*]three[/list]"] = "This is a list. <ol><li>one</li><li>two</li><li>three</li></ol>";
+    //simple list with multiple items - upper case codes
+    tests["This is a list. [LIST][*]one[*]two[*]three[/LIST]"] = "This is a list. <ol><li>one</li><li>two</li><li>three</li></ol>";
+    //simple list with multiple items - upper case first letter in codes
+    tests["This is a list. [List][*]one[*]two[*]three[/List]"] = "This is a list. <ol><li>one</li><li>two</li><li>three</li></ol>";
+    //simple list with multiple items - upper and lower case alternate
+    tests["This is a list. [LiSt][*]one[*]two[*]three[/lIsT]"] = "This is a list. <ol><li>one</li><li>two</li><li>three</li></ol>";
+    //incomplete list - code should refuse to change text
+    tests["Incomplete [list][*]one[*]two[*]three"] = "Incomplete [list][*]one[*]two[*]three";
+    //list without items should not be parsed
+    tests["List without items [list]incoming[/list]"] = "List without items [list]incoming[/list]";
+    //Nested lists should be just fine.
+    tests["I heard you like lists, so [list][*]I [list][*]made[*]a[*]list[/list][*]inside[*]your list.[/list]"]
+          = "I heard you like lists, so <ol><li>I <ol><li>made</li><li>a</li><li>list</li></ol></li><li>inside</li><li>your list.</li></ol>";
+    //list with line breaks between items - line breaks should not be touched here
+    tests[std::string("Hey!\n")
+          +"[list]\n"
+          +"[*]once\n"
+          +"[*]there\n"
+          +"[*]was a\n"
+          +"[*]crash test dummy,\n"
+          +"[*]who felt crashs ain't funny.\n"
+          +"[/list]"] = std::string("Hey!\n<ol>\n<li>once\n</li><li>there\n</li><li>was a\n</li>")
+                       +"<li>crash test dummy,\n</li><li>who felt crashs ain't funny.\n</li></ol>";
+
+    //iterate over all given strings and check, if they get the expected result
+    std::map<std::string, std::string>::const_iterator iter = tests.begin();
+    while (iter != tests.end())
+    {
+      std::string text = iter->first;
+      listCode_ordered.applyToText(text);
+      if (text != iter->second)
+      {
+        std::cout << "Error: Some code was not transformed properly!\n"
+                  << "Instance: ListBBCode(\"list\", false)\n"
+                  << "Original text:   \""<<iter->first<<"\"\n"
+                  << "Expected result: \""<<iter->second<<"\"\n"
+                  << "Actual result:   \""<<text<<"\"\n";
+        return 1;
+      }
+      ++iter;
+    } //while
+    std::cout << "Passed all ordered list code tests.\n";
+  } //end of scope for ordered list tests
+
+  return 0;
+}

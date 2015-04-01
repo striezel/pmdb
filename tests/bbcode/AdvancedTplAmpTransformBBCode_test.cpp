dfa5bf4e829f91838505f6f4c0efc29930bbe001
Thoronador <thoronador@users.sourceforge.net>
2015-04-01 23:33:03 +0200

    add test for AdvancedTplAmpTransformBBCode class

---
+++ tests/bbcode/AdvancedTplAmpTransformBBCode_test.cpp
@@ -0,0 +1,91 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the Private Message Database test suite.
+    Copyright (C) 2015  Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+#include "../../code/bbcode/AdvancedTplAmpTransformBBCode.hpp"
+
+int main()
+{
+  //fictious tag <tag>
+  AdvancedTplAmpTransformBBCode advancedTransform("tag", MsgTemplate("<tag value=\"{..attr..}\">{..inner..}</tag>"), "inner", "attr");
+
+  // Populate map with strings for testing.
+  //   Keys:   input string
+  //   Values: expected output string
+  std::map<std::string, std::string> tests;
+  //empty string stays unchanged
+  tests[""] = "";
+  //unchanged, no code
+  tests["There is no code here."] = "There is no code here.";
+  //simple code
+  tests["[tag=abc]123[/tag]"] = "<tag value=\"abc\">123</tag>";
+  //code with upper case letters
+  tests["[TAG=abc]123[/TAG]"] = "<tag value=\"abc\">123</tag>";
+  //only first letter in upper case
+  tests["[Tag=abc]123[/Tag]"] = "<tag value=\"abc\">123</tag>";
+  //mixed case code
+  tests["[TaG=def]123[/tAg]"] = "<tag value=\"def\">123</tag>";
+  //letters inside
+  tests["[tag=qwert]abcdefgh[/tag]"] = "<tag value=\"qwert\">abcdefgh</tag>";
+  //HTML entity inside - "&" should not get escaped
+  tests["[tag=aaa]&uuml;[/tag]"] = "<tag value=\"aaa\">&uuml;</tag>";
+  //HTML entity inside - "&" should not get escaped
+  tests["[tag=aaa]&amp;[/tag]"] = "<tag value=\"aaa\">&amp;</tag>";
+  //HTML entity inside - "&" should get escaped
+  tests["[tag=baz]foo&quot;bar[/tag]"] = "<tag value=\"baz\">foo&quot;bar</tag>";
+  //HTML entity inside attr. - "&" should get escaped there
+  tests["[tag=a&aa]mumble[/tag]"] = "<tag value=\"a&amp;aa\">mumble</tag>";
+  //HTML entity inside attr. - "&" should not get escaped
+  tests["[tag=&amp;]aaa[/tag]"] = "<tag value=\"&amp;amp;\">aaa</tag>";
+  //HTML entity inside attr. - "&" should get escaped
+  tests["[tag=ba&oink;z]foo&quot;bar[/tag]"] = "<tag value=\"ba&amp;oink;z\">foo&quot;bar</tag>";
+  //less than inside - should not get escaped
+  tests["[tag=baz]foo<bar[/tag]"] = "<tag value=\"baz\">foo<bar</tag>";
+  //greater than inside - should not get escaped
+  tests["[tag=baz]foo>bar[/tag]"] = "<tag value=\"baz\">foo>bar</tag>";
+  //another tag inside - should not get escaped
+  tests["[tag=asd]<foo>bar[/tag]"] = "<tag value=\"asd\"><foo>bar</tag>";
+  //start code only - should not change
+  tests["[tag=abc]123"] = "[tag=abc]123";
+  //end code only - should not change
+  tests["123[/tag]"] = "123[/tag]";
+  //incomplete again - no change
+  tests["[tag=abc]123[/tag"] = "[tag=abc]123[/tag";
+
+  //iterate over all given strings and check, if they get the expected result
+  std::map<std::string, std::string>::const_iterator iter = tests.begin();
+  while (iter != tests.end())
+  {
+    std::string text = iter->first;
+    advancedTransform.applyToText(text);
+    if (text != iter->second)
+    {
+      std::cout << "Error: Some code was not transformed properly!\n"
+                << "Instance: AdvancedTplAmpTransformBBCode(\"tag\", ..., \"inner\", \"attr\")\n"
+                << "Original text:   \"" << iter->first << "\"\n"
+                << "Expected result: \"" << iter->second << "\"\n"
+                << "Actual result:   \"" << text << "\"\n";
+      return 1;
+    }
+    ++iter;
+  } //while
+  std::cout << "Passed all AdvancedTplAmpTransformBBCode code tests.\n";
+  return 0;
+}

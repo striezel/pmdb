5056cb9890957e61cfd5c8a73a3bb342586e0296
Thoronador <thoronador@users.sourceforge.net>
2015-04-09 02:34:49 +0200

    add test for MessageDatabase::saveMessages()/loadMessages()

---
+++ tests/messagedatabase/save-load/save-load-test.cpp
@@ -0,0 +1,113 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the Private Message Database test suite.
+    Copyright (C) 2015  Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+#include "../../../code/MessageDatabase.hpp"
+
+// tests for MessageDatabase::saveMessages() and MessageDatabase::loadMessages()
+
+int main(int argc, char **argv)
+{
+  std::string tempDir;
+  if ((argc==2) and (argv!=NULL))
+  {
+    if (argv[1]!=NULL)
+    {
+      tempDir = std::string(argv[1]);
+    }//parameter exists
+    else
+    {
+      std::cout << "Parameter at index 1 is NULL.\n";
+      return 1;
+    }
+  }//if arguments present
+  else
+  {
+    std::cout << "Error: This program needs the path of a temporary directory as parameter!\n";
+    return 1;
+  }
+
+
+  MessageDatabase mdb;
+
+  //some random content
+  PrivateMessage pm;
+  pm.setDatestamp("2007-06-14 12:34");
+  pm.setTitle("This is the title");
+  pm.setFromUser("Hermes");
+  pm.setFromUserID(234);
+  pm.setToUser("Poseidon");
+  pm.setMessage("This is a message.");
+
+  //add some PMs to the database
+  unsigned int i;
+  const unsigned int limit = 2000;
+  for (i=1; i <= limit; ++i)
+  {
+    //change user ID to get a "different" message
+    pm.setFromUserID(i);
+    //database should accept message
+    if (!mdb.addMessage(pm))
+    {
+      std::cout << "Error: Could not add message with user ID " << i << " to DB!\n";
+      return 1;
+    }
+  } //for
+  std::cout << "Test info: Added " << limit << " messages successfully!\n";
+
+
+  if (!mdb.saveMessages(tempDir))
+  {
+    std::cout << "Error: Could not save messages to \"" << tempDir << "\"!\n";
+    return 1;
+  }
+  uint32_t readMessages = 0, newMessages = 0;
+  if (!mdb.loadMessages(tempDir, readMessages, newMessages))
+  {
+    std::cout << "Error: Could not load messages from \"" << tempDir << "\"!\n";
+    return 1;
+  }
+  if ((readMessages != limit) || (newMessages != 0))
+  {
+    std::cout << "Error: Unexpected message count!\n"
+              << "Expected " << limit << " messages in total (got "
+              << readMessages << ") and zero new messages (got "
+              << newMessages << ").\n";
+    return 1;
+  }
+  //now clear and do the same again
+  mdb.clear();
+  if (!mdb.loadMessages(tempDir, readMessages, newMessages))
+  {
+    std::cout << "Error: Could not load messages from \"" << tempDir << "\" a second time!\n";
+    return 1;
+  }
+  if ((readMessages != limit) || (newMessages != limit))
+  {
+    std::cout << "Error: Unexpected message count!\n"
+              << "Expected " << limit << " messages in total (got "
+              << readMessages << ") and " << limit << " new messages (got "
+              << newMessages << ").\n";
+    return 1;
+  }
+
+  std::cout << "Passed all MessageDatabase::saveMessages()/MessageDatabase::loadMessages() tests.\n";
+  return 0;
+}
---
+++ tests/messagedatabase/save-load/save-load-test.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# This file is part of the test suite for Private Message Database.
+# Copyright (C) 2015  Thoronador
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# check parameter
+if [[ -z $1 ]]
+then
+  echo "Error: Expecting one parameter - executable for this test!"
+  return 1
+fi
+
+# create base directory where the test shall take place
+SAVE_DIR=`mktemp --directory --tmpdir test-mdb-save-loadXXXXXXXXXX`
+
+# run test
+$1 $SAVE_DIR
+# ...and save its exit code
+TEST_EXIT_CODE_FIRST=$?
+
+# run test with slash at end of path
+$1 $SAVE_DIR/
+# ...and save its exit code
+TEST_EXIT_CODE_SECOND=$?
+
+TOTAL=0
+
+if [[ $TEST_EXIT_CODE_FIRST -ne 0 ]]
+then
+  echo "ERROR: The first part of the MessageDatabase save/load test (path without slash) failed!"
+  TOTAL=1
+fi
+
+if [[ $TEST_EXIT_CODE_SECOND -ne 0 ]]
+then
+  echo "ERROR: The second part of the MessageDatabase save/load test (path with slash) failed!"
+  TOTAL=2
+fi
+
+# clean up test directory
+rm -rf $SAVE_DIR
+
+if [[ $TOTAL -eq 0 ]]
+then
+  exit 0
+else
+  exit 1
+fi
